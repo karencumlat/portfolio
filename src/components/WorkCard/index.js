05e9af47213008ba2ccc55783aa4a30ce2c08b3f
karencumlat <karencumlat@gmail.com>
2021-02-25 16:25:30 -0800

    add(link): workcard

--- src/components/WorkCard/index.js
+++ src/components/WorkCard/index.js
@@ -3,19 +3,21 @@ import './workCard.css';
 function WorkCard(props) {
   return (
     <div className="app-card">
-      <img
-        src={process.env.PUBLIC_URL + props.url}
-        alt={props.alt}
-        className="app-card--image"
-        loading="lazy"
-      />
-      {props.link !== '' ? (
+      <a href={props.link} title={props.link}>
+        <img
+          src={process.env.PUBLIC_URL + props.url}
+          alt={props.alt}
+          className="app-card--image"
+          loading="lazy"
+        />
+      </a>
+      {props.link.includes('behance') ? (
         <span className="app-card--title">
-          {props.name} <a href={props.link}>{props.type}</a>
+          <a href={props.link}>{props.name}</a> - {props.year} - {props.type}
         </span>
       ) : (
         <span className="app-card--title">
-          {props.name} - {props.year} - {props.type}
+          {props.name} <a href={props.link}>{props.type}</a>
         </span>
       )}
     </div>
